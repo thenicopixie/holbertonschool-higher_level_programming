8993ea56396e42c46ec75cdb3d6fa400e33a62ec
thenicopixie <391@holbertonschool.com>
2018-09-11 18:18:47 +0000

    fixed tab spaces

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,12 +4,12 @@ number = random.randint(-10000, 10000)
 i = number
 if number < 0:
     i = number * -1
-last_dig = i%10
+last_dig = i % 10
 if last_dig > 5:
     message = "and is greater than 5"
 elif last_dig == 0:
     message = "and is zero"
 elif last_dig < 6:
-	if last_dig != 0:
-	    message = "and is less than 6 and not 0"
+    if last_dig != 0:
+        message = "and is less than 6 and not 0"
 print("Last digit of {} is {} {}".format(number, last_dig, message))
