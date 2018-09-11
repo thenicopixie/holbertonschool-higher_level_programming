57e9c0d3013ad777f30dcc8b097c370889c19240
thenicopixie <391@holbertonschool.com>
2018-09-11 19:31:53 +0000

    change mod for negative number to -10

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-i = number
+last_dig = number%10
 if number < 0:
-    i = number * -1
-last_dig = i % 10
+    last_dig = number%-10
 if last_dig > 5:
     message = "and is greater than 5"
 elif last_dig == 0:
