6170fb6945824b7aa0eab368b9372f13402f9b62
thenicopixie <391@holbertonschool.com>
2018-09-11 19:39:35 +0000

    fix modulus for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_dig = number%10
-if number < 0:
-    last_dig = number%-10
+if number > 0:
+    last_dig = number % 10
+elif number <= 0:
+    last_dig = number % -10
 if last_dig > 5:
     message = "and is greater than 5"
 elif last_dig == 0:
