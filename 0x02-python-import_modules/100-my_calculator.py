aaeb7e84dc74264339f3c06225efca839daebb07
thenicopixie <391@holbertonschool.com>
2018-09-13 21:57:20 +0000

    assign variables after chaging for number of arguments

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,8 +3,6 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
     op = {"+": add, "-": sub, "*": mul, "/": div}
-    a = int(argv[1])
-    b = int(argv[3])
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -13,4 +11,6 @@ if __name__ == "__main__":
         exit(1)
     for j in op:
         if argv[2] == j:
+            a = int(argv[1])
+            b = int(argv[3])
             print("{:d} {} {:d} = {:d}".format(a, j, b, op[j](a, b)))
