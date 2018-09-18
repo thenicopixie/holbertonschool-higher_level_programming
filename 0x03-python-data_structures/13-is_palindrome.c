8098b5ce5537338eb61678664a7ff45e7e8ed71f
thenicopixie <391@holbertonschool.com>
2018-09-18 22:35:02 +0000

    function to check if linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * is_palindrome - function that checks is a linked list is a palindrome
+ * @head: double pointer to a linked list
+ * Return: 1 if linked list is a palindrome, or 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp;
+	int str[3000];
+	int i, j;
+
+	if (!head)
+		return (1);
+	temp = *head;
+	i = 0;
+	while (temp)
+	{
+		str[i] = temp->n;
+		i++;
+		temp = temp->next;
+	}
+	i--;
+	for (j = 0; j <= i / 2 + 1; j++, i--)
+	{
+		if (str[i] == str[j])
+			continue;
+		else
+			return (0);
+	}
+	return (1);
+}
