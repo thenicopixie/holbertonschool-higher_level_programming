2bd171a14f14973074baa39fddf8adf7d08576d8
thenicopixie <391@holbertonschool.com>
2018-09-18 20:21:11 +0000

    set biggest to first index in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    biggest = 0
+    biggest = my_list[0]
     for i in my_list:
         if i > biggest:
             biggest = i
