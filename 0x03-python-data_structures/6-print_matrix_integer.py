74118685590d8b28e0eaac06f6025843ca279bda
thenicopixie <391@holbertonschool.com>
2018-09-18 18:14:37 +0000

    cast type to integer in print statement

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for rows in matrix:
         for columns in rows:
-            print("{}".format(columns), end="")
+            print("{:d}".format(columns), end="")
             if rows.index(columns) < len(rows) - 1:
                 print(" ", end="")
         print("\n", end="")
