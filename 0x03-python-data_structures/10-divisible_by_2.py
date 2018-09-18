cbdb5ec9e92deae8e93b56e3b2fac906f4b3662e
thenicopixie <391@holbertonschool.com>
2018-09-18 20:36:54 +0000

    add range in for loop

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return None
     new_list = my_list.copy()
-    for i in new_list:
+    for i in range(len(new_list)):
         if new_list[i] % 2 == 0:
             new_list[i] = True
         else:
