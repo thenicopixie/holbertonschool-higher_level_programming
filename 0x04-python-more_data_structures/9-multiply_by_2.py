b40bb67657dca08d4c75bab11c66ecf03726f267
thenicopixie <391@holbertonschool.com>
2018-09-20 21:40:19 +0000

    multiply dictionary values by 2, store in new dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] = a_dictionary[key]*2
-    return a_dictionary
+    b_dictionary = a_dictionary.copy()
+    for key in b_dictionary:
+        b_dictionary[key] = b_dictionary[key]*2
+    return b_dictionary
