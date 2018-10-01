a2a77b6c6b29b79a38c5ff12a28c5637a69659db
thenicopixie <391@holbertonschool.com>
2018-10-01 19:40:17 +0000

    divide two integers

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    diff = None
+    try:
+        diff = a / b
+    except ZeroDivisionError:
+        return diff
+    finally:
+        print("Inside Result: {}".format(diff))
+        return diff
