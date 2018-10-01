4f983e38283f42caa7317e4affb3878bf05b2f4f
thenicopixie <391@holbertonschool.com>
2018-10-01 19:41:43 +0000

    remove except built-in

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     diff = None
     try:
         diff = a / b
-    except ZeroDivisionError:
+    except:
         return diff
     finally:
         print("Inside Result: {}".format(diff))
