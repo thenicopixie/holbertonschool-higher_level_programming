ecd40c9d0c8a0c5081ef2c3d315e9c632dd95755
thenicopixie <391@holbertonschool.com>
2018-10-03 01:16:19 +0000

    fix spacing

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,13 +30,9 @@ class Square:
     @position.setter
     def position(self, value):
         """Setter for square position."""
-        if type(value) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[0]) is not int or type(value[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] < 0 or value[1] < 0:
+        if (type(value) is not tuple or type(value[0]) is not int
+                or type(value[1]) is not int or len(value) != 2 or
+                value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
