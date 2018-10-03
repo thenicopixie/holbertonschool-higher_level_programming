de5b83c14aea6e63ed097ded9c874bf210865802
thenicopixie <391@holbertonschool.com>
2018-10-03 00:47:27 +0000

    add check for int type

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,7 +30,13 @@ class Square:
     @position.setter
     def position(self, value):
         """Setter for square position."""
-        if len(value) is not 2 or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
