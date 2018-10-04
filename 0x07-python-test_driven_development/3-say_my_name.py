66fff6965298935914807ac78ce814e4a8598454
thenicopixie <391@holbertonschool.com>
2018-10-04 00:25:31 +0000

    function that prints a string

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module to print string"""
+
+def say_my_name(first_name, last_name=""):
+    """say_my_name - print string
+
+    Args:
+        first_name - first name to print
+        last_name - last name to print
+    """
+    if type(first_name) is not str or first_name is None:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str or last_name is None:
+        raise TypeError("last_name must be a string")
+    print("My name is ", end="")
+    if first_name is not "":
+        print("{} {}".format(first_name, last_name))
+    else:
+        print("{}{}".format(first_name, last_name))
