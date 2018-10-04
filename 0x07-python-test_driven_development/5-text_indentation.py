a535b612a094790da8fbca691693afb80aeabc36
thenicopixie <391@holbertonschool.com>
2018-10-04 04:31:21 +0000

    fix case for newline whitspaces

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,10 +7,15 @@ def text_indentation(text):
     Args:
     text - text to indent and print
     """
+    flag = 0
     if type(text) is not str:
         raise TypeError("text must be a string")
     for i in text:
         if i == '.' or i == '?' or i == ':':
             print("{}\n\n".format(i), end="")
+            flag = 1
+        elif i == " " and flag == 1:
+                pass
         else:
             print("{}".format(i), end="")
+            flag = 0
