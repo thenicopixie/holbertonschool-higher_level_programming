10eb6122c302026b4ff8dfb00d245188b65d30fb
thenicopixie <391@holbertonschool.com>
2018-10-04 01:01:18 +0000

    add case for size of 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,6 +12,8 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if size == 0:
+        print()
     row = "#" * size
     for i in range(size):
         print("{}".format(row))
