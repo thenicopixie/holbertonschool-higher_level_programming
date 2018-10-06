00e572fb72e5950c96997205af502dfc1675bdbf
thenicopixie <391@holbertonschool.com>
2018-10-06 16:14:50 +0000

    remove None case

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,8 +9,8 @@ def say_my_name(first_name, last_name=""):
         first_name - first name to print
         last_name - last name to print
     """
-    if type(first_name) is not str or first_name is None:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str or last_name is None:
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
