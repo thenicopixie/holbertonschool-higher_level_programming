8d408986e6321bc983de0a4f39041db437d03cb5
thenicopixie <391@holbertonschool.com>
2018-10-10 22:21:39 +0000

    numpy multiplication function

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Import numpy module """
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Return numpy matrix multiplication """
+    return np.matmul(m_a, m_b)
