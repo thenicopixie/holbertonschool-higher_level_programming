08b7c3a68556d79646bc0b746b1271ae9561f12a
thenicopixie <391@holbertonschool.com>
2018-10-10 17:24:46 +0000

    use slots method for locked case

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,11 +4,4 @@
 
 class LockedClass:
     """ Base LockedClass class"""
-
-    def __setattr__(self, key, value):
-        """
-        Set attribute if key is found
-        """
-        string = "'LockedClass' object has no attribute "
-        if key is not "first_name":
-            raise AttributeError("{}'{}'".format(string, key))
+    __slots__ = ["first_name"]
