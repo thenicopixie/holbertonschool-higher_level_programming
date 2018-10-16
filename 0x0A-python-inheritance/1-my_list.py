55fb1ef4078502fd18c5a3a9e72e02067d162ffc
thenicopixie <391@holbertonschool.com>
2018-10-16 07:45:45 +0000

    remove init method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,6 @@
 
 class MyList(list):
     """Subclass MyList that inherits from list"""
-    def __init__(self):
-        """Initialize list"""
-        super().__init__()
 
     def print_sorted(self):
         """Print sorted list"""
