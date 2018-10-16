90c0dbdd4445e0a5a6fe812402882bed97974e25
thenicopixie <391@holbertonschool.com>
2018-10-16 20:15:38 +0000

    deserialize a json string

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that returns an object represented by a JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """Return an object represented by a JSON string"""
+    return json.loads(my_str)
