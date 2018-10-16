c23460330322e6abace09c1855e6846d487c9041
thenicopixie <391@holbertonschool.com>
2018-10-16 07:31:49 +0000

    fix typo

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ Module to check if object is an instace of a inherited class
 """
 
@@ -9,6 +9,7 @@ def inherits_from(obj, a_class):
     Args:
     obj: object to check
     a_class: class to check for instance"""
-    if type(obj) is a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    else:
         return False
-    return isinstance(obj, a_class)
