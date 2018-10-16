8faa28ecc16d3208c1e093cf4dd9a1fc56ee6870
thenicopixie <391@holbertonschool.com>
2018-10-16 23:58:19 +0000

    check if attributes are in dictionary

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Creates a class that defines a student"""
+
+
+class Student:
+    """Base class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialization of attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a class instance"""
+        new_dict = {}
+        try:
+            for i in attrs:
+                if self.__dict__.get(i) is not None:
+                    new_dict.update({i: self.__dict__.get(i)})
+            return new_dict
+        except:
+            return self.__dict__
