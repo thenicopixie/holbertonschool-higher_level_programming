676deeb11e4488718e6a8c088d63cd055d893310
thenicopixie <391@holbertonschool.com>
2018-10-17 18:16:21 +0000

    import class from file, print square description

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,44 +1,6 @@
 #!/usr/bin/python3
 """Module for base class base geometry and subclass rectangle"""
-
-
-class BaseGeometry:
-    """Base class BaseGeometry that raises an exception
-    with a message, and validates a value"""
-    def area(self):
-        """Method that raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate an integer
-        Args:
-        name: string
-        value: integer to validate
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Class that inherites from BaseGeometry.
-    Intializes width and height"""
-    def __init__(self, width, height):
-        """Method to initialize width and height"""
-        self.integer_validator('width', width)
-        self.__width = width
-        self.integer_validator('height', height)
-        self.__height = height
-
-    def area(self):
-        """Method to get area of rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Print area of rectangle"""
-        c_name = self.__class__.__name__
-        return("[{}] {}/{}".format(c_name, self.__width, self.__height))
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
@@ -46,4 +8,11 @@ class Square(Rectangle):
     Initializes size"""
     def __init__(self, size):
         """Initialize size"""
+        self.integer_validator('size', size)
+        self.__size = size
         super().__init__(size, size)
+
+    def __str__(self):
+        """Print square description"""
+        class_name = self.__class__.__name__
+        return "[{}] {}/{}".format(class_name, self.__size, self.__size)
