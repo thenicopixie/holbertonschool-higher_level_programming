277648363cac8d4cf2a1516e0efdee7ff4e68715
thenicopixie <391@holbertonschool.com>
2018-10-18 23:49:22 +0000

    use attributes from rectangle class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,16 +7,13 @@ class Square(Rectangle):
     """Class Square that inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize size, x, y, and id"""
-        self.__size = size
-        self.__x = x
-        self.__y = y
-        self.id = id
-        self = super().__init__(size, size, x, y, id)
+        Rectangle.__init__(self, size, size, x=x, y=y, id=id)
+        self.size = size
 
     def __str__(self):
         """Return message to stdout"""
         id_s = self.id
-        s_s = self.__size
-        x_s = self.__x
-        y_s = self.__y
+        s_s = self.width
+        x_s = self.x
+        y_s = self.y
         return "[Square] ({}) {}/{} - {}".format(id_s, x_s, y_s, s_s)
