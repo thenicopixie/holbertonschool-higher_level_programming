7fe119ed7205f0798aa2320cac420eab21501afd
thenicopixie <391@holbertonschool.com>
2018-10-19 19:36:54 +0000

    add method that returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for base class Base"""
+import json
 
 
 class Base:
@@ -13,3 +14,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of
+        list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
