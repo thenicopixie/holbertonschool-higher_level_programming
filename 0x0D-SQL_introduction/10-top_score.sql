397eeab8b96e4b856b98b7646a2d8a5af28eefb9
thenicopixie <391@holbertonschool.com>
2018-12-03 21:51:03 +0000

    fix typo

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- Script that lists all records of the table second_table of the database
 -- hbtn_0c_0 in the MySQL server
-SELECT id, name FROM second_table ORDER BY score DESC
+SELECT score, name FROM second_table ORDER BY score DESC
 
