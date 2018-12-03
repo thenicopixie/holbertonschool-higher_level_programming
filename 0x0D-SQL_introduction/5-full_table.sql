d87b4804c0598729150575ee64cd011661cde391
thenicopixie <391@holbertonschool.com>
2018-12-03 20:42:48 +0000

    script that prints the full description of the given table from the given database of the MySQL server

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- This script prints the full description of the given table from the
+-- given database of the MySQL server
+-- Not allowed to use the DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE first_table
