41c38402987199e0207543c3fc0d28723165b17a
thenicopixie <391@holbertonschool.com>
2018-12-03 19:03:47 +0000

    script that deletes the given database if it exists

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- This script deletes the database from the MySQL server
+-- If the given databse doesn't exist, do not show failure
+-- Not allowed to use the SELECT of SHOW statements
+DROP DATABASE IF EXISTS hbtn_0c_0
