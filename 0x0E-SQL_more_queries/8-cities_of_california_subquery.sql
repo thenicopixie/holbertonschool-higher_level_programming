074c986b81623ce5558abe05a3ecdd2d9de3d2b5
thenicopixie <391@holbertonschool.com>
2018-12-04 23:47:51 +0000

    change select to only select id and name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- This script lists all the cities of California that can be found in
 -- the database hbtn_0d_usa
-SELECT * FROM cities WHERE id =
+SELECT id, name FROM cities WHERE id =
     (SELECT state_id FROM states WHERE name = 'California')
 ORDER BY cities.id;
