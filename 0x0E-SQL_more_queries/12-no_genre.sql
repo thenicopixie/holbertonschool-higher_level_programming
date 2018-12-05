be3cf488c16d7e0ef00cd92915fe103b2b3bb39f
thenicopixie <391@holbertonschool.com>
2018-12-05 01:04:37 +0000

    script that lists all shows contained in a database without a genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- This script lists all shows contained hbtn_0d_tvshows without a genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+GROUP BY tv_shows.title, tv_show_genres.genre_id;
