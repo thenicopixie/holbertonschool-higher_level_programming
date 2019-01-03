3b6fad585785ae3f840eeae5f1c5d618f4765a32
thenicopixie <391@holbertonschool.com>
2019-01-03 00:26:17 +0000

    create state class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script that lists all cities of a state from a database
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """A state class inheriting from base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
