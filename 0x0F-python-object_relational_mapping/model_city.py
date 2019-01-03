4f376ca357034337846d76776bd959d96e5733d8
thenicopixie <391@holbertonschool.com>
2019-01-03 20:17:38 +0000

    create a City class that inherits from Base

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains the calss definition of a City
+"""
+if __name__ == "__main__":
+    from sqlalchemy import Column, Integer, String
+    from model_state import Base
+
+    class City(Base):
+        """A state class inheriting from base
+        """
+        __tablename__ = 'cities'
+        id = Column(Integer, primary_key=True, autoincrement=True,
+                    nullable=False)
+        name = Column(String(50), nullable=False)
+        state_id = Column(Integer, foreign_key(states.id), nullable=False)
