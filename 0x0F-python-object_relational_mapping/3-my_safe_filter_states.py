db96057bf8e37284eeada79575007a0a2e369b51
thenicopixie <391@holbertonschool.com>
2019-01-03 05:05:51 +0000

    import guard

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Script that lists all states with a name starting with N
 safe from injections"""
-import MySQLdb
-import sys
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-cur = conn.cursor()
-cmd = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-cur.execute(cmd, (
-            sys.argv[4], ))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cmd = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(cmd, (
+                sys.argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
