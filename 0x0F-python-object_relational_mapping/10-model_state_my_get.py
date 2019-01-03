a255ff846322340e9787602462f92e6d9adebdd7
thenicopixie <391@holbertonschool.com>
2019-01-03 04:49:32 +0000

    import guard

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,24 +2,24 @@
 """Print the state object with the name passed as the argument from the
 database
 """
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
-import sys
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State, Base
+    import sys
 
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3],
+                           sys.argv[4]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                       sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]),
-                       pool_pre_ping=True)
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-flag = 0
-for state in session.query(State).all():
-    if state.name == sys.argv[4]:
-        flag = 1
-        print("{}".format(state.id))
-if flag == 0:
-    print("Not found")
-session.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    flag = 0
+    for state in session.query(State).all():
+        if state.name == sys.argv[4]:
+            flag = 1
+            print("{}".format(state.id))
+    if flag == 0:
+        print("Not found")
+    session.close()
