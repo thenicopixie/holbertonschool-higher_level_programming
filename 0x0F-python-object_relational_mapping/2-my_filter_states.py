b56da81785f9207d46987f41f47ede0a79b906e2
thenicopixie <391@holbertonschool.com>
2019-01-03 05:08:04 +0000

    import guard

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Script that lists all states with a name starting with N"""
-import MySQLdb
-import sys
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-            sys.argv[4]))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states WHERE name = '{}'
+                ORDER BY id ASC""".format(sys.argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
