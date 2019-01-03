58fe9b05f3e9fe9b8264f542ff33b049f888ecdd
thenicopixie <391@holbertonschool.com>
2019-01-03 23:27:27 +0000

    City class that inherits from Base

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains the class definition of a City
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """A state class inheriting from base
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False)
+    name = Column(String(50), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
