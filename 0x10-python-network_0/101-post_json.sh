e0521c01da469e817dc9078757f39cdb509d4d3f
thenicopixie <391@holbertonschool.com>
2019-01-16 07:11:28 +0000

    fix xpost

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL.
 # Displays the body of the response
-curl -s -H "Content-Type: application/json" -X POST -d @$2 $1
+curl -s  -X POST $1 -H "Content-Type: application/json" -d @$2
