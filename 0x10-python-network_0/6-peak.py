9017aea6b75fe11e606c4f49e06c77bb03fa9ab6
thenicopixie <391@holbertonschool.com>
2019-01-16 00:37:47 +0000

    find peak in list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """go through list to find a peak"""
+    nums = list_of_integers
+    if nums is None:
+        return None
+    ln = len(nums) - 1
+    if ln % 2 != 0:
+        ln + 1
+    point = int(ln/2)
+    l = point - 1
+    r = point + 1
+    if l >= 0 and r <= ln:
+        if nums[point] >= nums[l] and nums[point] >= nums[r]:
+            return nums[point]
+        else:
+            if nums[l] >= nums[point]:
+                return find_peak(nums[0:point+1])
+            if nums[r] >= nums[point]:
+                return find_peak(nums[point:ln+1])
+    elif l < 0:
+        if r <= ln:
+            if nums[point] >= nums[r]:
+                return nums[point]
+    elif r > ln:
+        if l >= 0:
+            if nums[point] >= nums[l]:
+                return nums[point]
