aac1ea602af91d5f642cd740068046157b2ef0d6
thenicopixie <391@holbertonschool.com>
2019-01-16 07:15:26 +0000

    fix shellcheck

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL.
-# Displays the body of the response
-curl -s  -X POST $1 -H "Content-Type: application/json" -d @$2
+curl -s -H "Content-Type: application/json" -X POST -d @"$2" "$1"
