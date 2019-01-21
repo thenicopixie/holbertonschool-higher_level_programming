36f3f9011992cb65afdfec1cbd9fa4af2253781e
thenicopixie <391@holbertonschool.com>
2019-01-21 22:03:00 +0000

    fix typo

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,7 @@ import urllib.request
 import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         html = dict(response.info())
 
