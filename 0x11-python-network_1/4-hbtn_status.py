6349df331a6351b1e2eee37e2e4b378cb93520fa
thenicopixie <391@holbertonschool.com>
2019-01-22 01:54:31 +0000

    fetch request using requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetches a URL using request"""
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(req.text))
+    print("\t- content: {}".format(req.content.decode('utf-8')))
