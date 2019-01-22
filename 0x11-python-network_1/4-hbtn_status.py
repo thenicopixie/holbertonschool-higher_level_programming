813bff1328271bc382bacc93fdd8747beb48b16f
thenicopixie <391@holbertonschool.com>
2019-01-22 01:55:56 +0000

    get type of request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     req = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(req.text))
+    print("\t- type: {}".format(type(req.text)))
     print("\t- content: {}".format(req.content.decode('utf-8')))
