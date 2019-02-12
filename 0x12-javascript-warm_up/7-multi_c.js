aed9627c2b654fdf18c3d07b6bb2a5c2f750899c
thenicopixie <391@holbertonschool.com>
2019-02-12 22:19:39 +0000

    fix style, change ternary

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let x = parseInt(process.argv[2]);
-function c(num) {
-  while(num > 0) {
+function c (num) {
+  while (num > 0) {
     console.log('C is fun');
     num--;
   }
 }
-isNaN(x) ? console.log('Missing number of occurrences') : c(x);
+x ? c(x) : console.log('Missing number of occurrences');
