cfbeba7d67bf04ee3ffd63e376520d0bbcddccee
thenicopixie <391@holbertonschool.com>
2019-02-12 20:40:53 +0000

    modify, not allowed to use length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+let count = 0;
+process.argv.forEach ((val, index) => {
+  count += 1;
+});
+if (count === 2) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
