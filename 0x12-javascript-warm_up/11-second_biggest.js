a22f96e8a468762da02a9877987a25102f9498f2
thenicopixie <391@holbertonschool.com>
2019-02-12 23:24:06 +0000

    fix sort to sort by interger

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 let len = process.argv.length;
+
+function sorted(a, b) {
+  return (a - b);
+}
 if (len === 2 || len === 3) {
   console.log(0);
 } else {
-  const args = process.argv.slice(2).sort();
+  let args = process.argv.slice(2).sort(sorted);
   console.log(args[args.length - 2]);
 }
