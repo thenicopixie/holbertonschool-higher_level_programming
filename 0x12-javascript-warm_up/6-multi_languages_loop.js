5763906b6c8bd4e88cc2dea359a93030a73d01f5
thenicopixie <391@holbertonschool.com>
2019-02-12 21:25:51 +0000

    fix styling

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-var array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (var i = 0; i < array.length; i++) {
+let array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
 }
