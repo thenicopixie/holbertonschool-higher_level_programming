785cb9e627b4666850af7ff0d849988a40b1a9b8
thenicopixie <391@holbertonschool.com>
2019-02-19 21:33:59 +0000

    change url search

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ the character "Wedge Antilles" is present */
 const request = require('request');
 const url = process.argv[2];
 request(url, function (err, res, body) {
-  const wedge = 'https://swapi.co/api/people/18/';
+  const wedge = '18/';
   let count = 0;
   if (err) { console.log(err); } else {
     let result = JSON.parse(body).results;
