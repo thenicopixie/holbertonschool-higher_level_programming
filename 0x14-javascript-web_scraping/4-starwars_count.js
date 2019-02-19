f26c41cffae618b8a71b13b73afb9d4e36a329f3
thenicopixie <391@holbertonschool.com>
2019-02-19 20:54:53 +0000

    change variable name

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,10 +6,10 @@ let url = process.argv[2];
 let wedge = 'https://swapi.co/api/people/18/';
 request(url, function (err, res, body) {
   if (err) { console.log(err); }
-  let response = JSON.parse(body).results;
+  let result = JSON.parse(body).results;
   let count = 0;
-  for (let i = 0; i < response.length; i++) {
-    let characters = response[i].characters;
+  for (let i = 0; i < result.length; i++) {
+    let characters = result[i].characters;
     for (let j = 0; j < characters.length; j++) {
       if (characters[j] === wedge) {
         count += 1;
