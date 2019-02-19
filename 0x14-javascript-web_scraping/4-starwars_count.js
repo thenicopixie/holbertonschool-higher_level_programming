0d87caa54dc81c3a0088a2e2de26a0dbc387abef
thenicopixie <391@holbertonschool.com>
2019-02-19 21:02:48 +0000

    move variable inside function

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,15 @@
 the character "Wedge Antilles" is present */
 const request = require('request');
 let url = process.argv[2];
-let wedge = 'https://swapi.co/api/people/18/';
 request(url, function (err, res, body) {
+  let wedge = 'https://swapi.co/api/people/18/';
   if (err) { console.log(err); }
   let result = JSON.parse(body).results;
   let count = 0;
   for (let i = 0; i < result.length; i++) {
-    let characters = result[i].characters;
-    for (let j = 0; j < characters.length; j++) {
-      if (characters[j] === wedge) {
+    let people = result[i].characters;
+    for (let j = 0; j < people.length; j++) {
+      if (people[j] === wedge) {
         count += 1;
       }
     }
