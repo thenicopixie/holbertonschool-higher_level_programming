623875d171445bb097d7a25dc05bff60dbb0861b
thenicopixie <391@holbertonschool.com>
2019-02-19 18:07:25 +0000

    add else statement

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,5 +3,5 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, contents) {
   if (err) { console.log(err); }
-  console.log(contents);
+  else { console.log(contents); }
 });
