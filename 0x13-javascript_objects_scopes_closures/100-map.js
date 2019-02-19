3ada65a45b643d9f1b0c5dce131737f63f4d8c43
thenicopixie <391@holbertonschool.com>
2019-02-19 00:16:50 +0000

    change names to camelcase

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* Script that imports an array and computes a new array */
-const a_list = require('./100-data.js').list;
+const aList = require('./100-data.js').list;
 let i = 0;
-const new_list = a_list.map(function (num) {
+const newList = aList.map(function (num) {
   return num * i++;
 });
-console.log(a_list);
-console.log(new_list);
+console.log(aList);
+console.log(newList);
