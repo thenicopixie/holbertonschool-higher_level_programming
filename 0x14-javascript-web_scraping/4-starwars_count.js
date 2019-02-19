4358985d54fea9f362abf1e90951e9e3870937a0
thenicopixie <391@holbertonschool.com>
2019-02-19 21:05:52 +0000

    add else statement

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,19 +2,20 @@
 /* Script that prints the number of movies where
 the character "Wedge Antilles" is present */
 const request = require('request');
-let url = process.argv[2];
+const url = process.argv[2];
 request(url, function (err, res, body) {
-  let wedge = 'https://swapi.co/api/people/18/';
-  if (err) { console.log(err); }
-  let result = JSON.parse(body).results;
-  let count = 0;
-  for (let i = 0; i < result.length; i++) {
-    let people = result[i].characters;
-    for (let j = 0; j < people.length; j++) {
-      if (people[j] === wedge) {
-        count += 1;
+  const wedge = 'https://swapi.co/api/people/18/';
+  if (err) { console.log(err); } else {
+    let result = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i < result.length; i++) {
+      let people = result[i].characters;
+      for (let j = 0; j < people.length; j++) {
+        if (people[j] === wedge) {
+          count += 1;
+        }
       }
     }
+    console.log(count);
   }
-  console.log(count);
 });
